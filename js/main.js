03ee74877adbfea31a4c43bead64ecae50b2f415
jing.wang <admin@jingwangnet.com>
2020-05-13 08:50:48 +0800

    add role to html
    Signed-off-by: jing.wang <admin@jingwangnet.com>

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+window.addEventListener('load', initAll);
+
+
+function initAll() {
+    btnSwith('#nav-search-btn', '#nav-search')
+    btnSwith('#nav-list-btn', '#nav-menu')
+}
+
+function btnSwith(btn, obj) {
+    let button = document.querySelector(btn);
+    let toDisplayElement = document.querySelector(obj);
+
+    if (toDisplayElement && button) {
+        button.addEventListener('click', () => {
+            button.classList.toggle("active");
+            toDisplayElement.classList.toggle("active");
+        });
+    } else {
+        console.log(`${obj} is toDisplayElement: ${toDisplayElement}`);
+    }
+}
